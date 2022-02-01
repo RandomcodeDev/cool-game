cd7b491323290efe67b35e85274e486dccbefcb7
TonKoopman <jacob.w.hodges25@gmail.com>
2022-01-31 19:13:52 -0500

    Added fullscreen toggler input

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from pygame.locals import *
 from settings import *
 from level import Level
 
+
 class Game:
     def __init__(self):
 
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
-        pygame.display.set_caption('Main')
+        pygame.display.set_caption("Main")
         self.clock = pygame.time.Clock()
 
     def run(self):
@@ -18,10 +19,16 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
-                self.screen.fill('black')
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_F11:
+                        # toggle fullscreen
+                        pass
+
+                self.screen.fill("black")
                 pygame.display.update()
                 self.clock.tick(FPS)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     game = Game()
     game.run()
--- settings.py
+++ settings.py
@@ -3,4 +3,6 @@ HEIGHT   = 720
 FPS      = 60
 TILESIZE = 64
 
-WORLD_MAP = []
+WORLD_MAP = [
+]
+
