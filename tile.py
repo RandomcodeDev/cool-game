b81cd7c3445583a7910a00b328cac99d660de123
TonKoopman <jacob.w.hodges25@gmail.com>
2022-01-31 20:39:29 -0500

    Tile map now works

---
+++ tile.py
@@ -0,0 +1,36 @@
+import pygame
+
+WORLD_MAP = [
+['l','b','r'],
+['l',',','r'],
+['l','f','r']
+]
+
+class tile:
+    BACK_WALL: str = 'b'
+    FRONT_WALL: str = 'f'
+    LEFT_WALL: str = 'l'
+    RIGHT_WALL: str = 'r'
+    EMPTY: str = ','
+    PLAYER: str = 'p'
+    TILESIZE: int = 64
+    def __init__(self, map):
+        self.map = map
+
+        self.display_surface = pygame.display.get_surface()
+
+    def draw_map(self):
+        for row_num,row in enumerate(self.map):
+            for col_num,column in enumerate(row):
+                x = col_num * tile.TILESIZE
+                y = row_num * tile.TILESIZE
+                if column == tile.BACK_WALL:
+                    pygame.draw.rect(self.display_surface, ('red'), (x, y, tile.TILESIZE, tile.TILESIZE))
+                elif column == tile.FRONT_WALL:
+                    pygame.draw.rect(self.display_surface, ('red'), (x, y, tile.TILESIZE, tile.TILESIZE))
+                elif column == tile.RIGHT_WALL:
+                    pygame.draw.rect(self.display_surface, ('red'), (x, y, tile.TILESIZE, tile.TILESIZE))
+                elif column == tile.LEFT_WALL:
+                    pygame.draw.rect(self.display_surface, ('red'), (x, y, tile.TILESIZE, tile.TILESIZE))
+                elif column == tile.EMPTY:
+                    pass
